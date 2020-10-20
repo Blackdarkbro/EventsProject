e1246015d2ef922597f4fe22a146f564d63a9f87
Savchik Egor <egorsavchik98@mail.ru>
2020-10-20 16:17:24 +0300

    Initial commit

---
+++ Assets/Scripts/InventoryGame/NewBehaviourScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour, IPointerClickHandler, IDragHandler,
+    IPointerEnterHandler, IPointerExitHandler, IEndDragHandler, IDropHandler
+{
+    [SerializeField] Transform playerInventory;
+    [SerializeField] Image defaultImg;
+    [SerializeField] Image shipImg;
+
+    private Vector2 cellPosition;
+    private Vector3 originCellPosition;
+    
+
+    private void Start() {
+        originCellPosition = transform.TransformVector(transform.position);
+        Debug.Log(transform.position);
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        transform.DOMove(originCellPosition, .5f);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+       
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(Vector2.one * 1.05f, 0.3f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+         transform.DOScale(Vector2.one, 0.3f);
+    }
+}
---
+++ Assets/Scripts/InventoryGame/Ship.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Ship
+{
+    public static string name = "Spaceship";
+    public static int price = 100;
+}
